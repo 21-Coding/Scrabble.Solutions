9acc0474d0f791d82b552b84c40761c4530c6fde
Dominique Youmans and Jamison Cozart <dominique+jamison@>
2020-03-05 11:20:43 -0800

    add new tests

--- Scrabble.Tests/ModelTests/ScrabbleTests.cs
+++ Scrabble.Tests/ModelTests/ScrabbleTests.cs
@@ -14,6 +14,12 @@ namespace Game.Tests
       int wordScore = newGame.CalculateWordScore("hello");
       Assert.AreEqual(8, wordScore);
     }
-
+    [TestMethod]
+    public void RandomLetter_ReturnsRandomLetter_()
+    {
+      Scrabble newGame = new Scrabble();
+      char[] charArray = newGame.RandomLetters();
+      Assert.AreEqual(7, charArray.Length);
+    }
   }
 }
